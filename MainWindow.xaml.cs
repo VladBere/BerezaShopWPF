1c02988ae698a8dbcb6e84fa07edb9bc5e09b4c2
VladBere <vladyslavbereza17@gmail.com>
2026-07-20 23:07:10 +0300

    Initialize main window and data collection

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,34 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Shop
+namespace BerezaShop
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<MenuItem> _items;
+        private decimal _tipAmount = 0m;
+
         public MainWindow()
         {
             InitializeComponent();
+            _items = new ObservableCollection<MenuItem>();
+            LvBill.ItemsSource = _items;
+            UpdateTotals();
         }
+
+        private void UpdateTotals()
+        {
+            LblNetTotal.Text = "$0.00";
+            LblTipAmount.Text = "$0.00";
+            LblGstAmount.Text = "$0.00";
+            LblTotalAmount.Text = "$0.00";
+        }
+    }
+
+    public class MenuItem
+    {
+        public string Description { get; set; }
+        public decimal Price { get; set; }
     }
 }
